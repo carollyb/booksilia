e55364dad04f75ed9b13188bbb4df6c6a16e25d0
carollyb <carolly.barbosa@gmail.com>
2022-04-20 17:43:22 -0300

    feat/signup-page: criação da estrutura da página

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,8 +1,52 @@
+import {
+    Text
+} from '@chakra-ui/react';
+import ContainerSection from '../components/Layouts/Container';
+import BoxSection from '../components/Layouts/Box';
+import InputComponent from '../components/Input/InputComponent';
+import ButtonComponent from '../components/Button/ButtonComponent';
+
 function SignUpPage() {
     return (
-        <h1>
-            signup
-        </h1>
+        <ContainerSection>
+            <BoxSection>
+            <Text
+            fontFamily={'sen'}
+            fontSize={'40px'}
+            color={'purple'}
+            fontWeight={'bold'}
+            >Cadastre-se</Text>
+            <Text
+            fontFamily={'sen'}
+            color={'lightGray'}
+            fontSize={'16px'}
+            >
+                para ter acesso ao sistema
+            </Text>
+            <InputComponent
+            id='fullName'
+            type="text"
+            placeholder={'Nome e Sobrenome'}
+            />
+            <InputComponent
+            id='username'
+            type="text"
+            placeholder={'Username'}
+            />
+            <InputComponent
+            id='password'
+            type="password"
+            placeholder={'Senha'}
+            />
+            <ButtonComponent
+            backgroundColor={'purple'}
+            color={'white'}
+            onClick={(e) => {handleSubmit(e)}}
+            >
+                Sign Up
+            </ButtonComponent>
+            </BoxSection>
+        </ContainerSection>
     );
 }
 
