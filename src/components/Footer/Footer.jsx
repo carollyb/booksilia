ae452a0c3c52797d3707a93d415c20273d104d20
Priscila Rossetto <priscilassouzap@gmail.com>
2022-04-23 18:16:54 -0300

    style/footer: implementa layout footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,55 +1,51 @@
-import { Box, Flex, Text, Link, HStack } from '@chakra-ui/react';
-import Container from '../Layouts/Container';
+import { Box, Container, Stack, Flex, Text, Link, SimpleGrid } from '@chakra-ui/react';
 import { FaInstagram, FaTwitter, FaLinkedin } from 'react-icons/fa';
 
 
 function FooterComponent() {
     return (
-        <Container>
-            <Box
-                bg="#fbd7b7b6"
-                width="100%"
-                height={'300px'}
-                justifyContent={"center"}
-                alignItems="space-between"
-                direction="column"
-                padding={6}>
-                <Text
-                    fontFamily={'Pacifico, cursive'}
-                    color={'purple'}
-                    fontSize={'36px'}
-                    fontWeight={'bold'}>Booksilia</Text>
-                <Box>
-                    <Flex gap={'10px'}>
-                        <Link href='#'>
-                            <FaInstagram />
-                        </Link>
-                        <Link href='#'>
-                            <FaTwitter />
-                        </Link>
-                        <Link href='#'>
-                            <FaLinkedin />
-                        </Link>
-                    </Flex>
-                </Box>
-                <Flex
-                    justify={'center'}
-                    textAlign={'center'}
-                    flexDirection='flex-end'>
-                    <HStack spacing={200}>
-                        <Text fontSize={'sm'}
-                            flexDirection='unset'>
-                            © 2022 Booksilia - Web Dev FullStack - Resilia Educação
-                        </Text>
+    <>
+        <Box
+            bg="#fbd7b7b6"
+            width="100%"
+            height={'200px'}>
+                <Container as={Stack} maxW={'6x1'} py={10}>
+                    <SimpleGrid
+                    templateColumns={{ sm: '1fr 1fr', md: '2fr 1fr 1fr 2fr'}}
+                    spacing={8}>
+                        <Stack spacing={6}>
+                            <Box>
+                            <Text
+                            fontFamily={'Pacifico, cursive'}
+                            color={'purple'}
+                            fontSize={'36px'}
+                            fontWeight={'bold'}>Booksilia</Text>
+                            </Box>
+                        </Stack>
+
+                    </SimpleGrid>
+
+                </Container>
+                
+                <Container
+                as={Stack}
+                maxW={'6xl'}
+                py={4}
+                direction={{ base: 'column', md: 'row' }}
+                spacing={4}
+                justify={{ base: 'center', md: 'space-between' }}
+                align={{ base: 'center', md: 'center' }}>
+                    <Text>© 2022 Booksilia - Web Dev FullStack - Resilia Educação</Text>
+                    <Stack direction={'row'} spacing={6}>
                         <Text fontSize={'sm'}
                             flexDirection='unset'>
                             Feito com ♡ pelo Grupo 5
                         </Text>
-                    </HStack>
-                </Flex>
-            </Box>
-        </Container>
-    );
+                    </Stack>
+                    
+            </Container>
+        </Box>
+    </>);
 }
 
 export default FooterComponent;
