44e6cfeb938725b3793b99a0ed0b113d2c3d836c
Priscila Rossetto <priscilassouzap@gmail.com>
2022-04-26 15:46:25 -0300

    feat/home: altera fonte

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -28,6 +28,7 @@ function Home() {
     const navigate = useNavigate()
 
     return (
+        <>
         <Container>
         <Flex display="flex" alignItems="center">
             <Flex alignItems="center">
@@ -38,76 +39,79 @@ function Home() {
                 boxShadow={'2xl'}
                 rounded={'md'}
                 p={6}
-                overflow={'hidden'}>
+                overflow={'hidden'}
+                >
                     <Text 
                     fontFamily={'sen'}
                     fontSize={'30px'}
                     color={'purple'}
-                    fontWeight={'bold'}>
+                    fontWeight={'bold'}
+                    paddingTop={20}>
                         Bem-vindo,
                     </Text>
                     <Button
-                backgroundColor={'purple'}
-                color={'white'}
-                onClick={() => navigate('/stock')}
-                >
-                    Acessar o estoque
-                </Button>
+                        fontFamily={'sen'}
+                        backgroundColor={'purple'}
+                        color={'white'}
+                        onClick={() => navigate('/stock')}>
+                            Acessar o estoque
+                    </Button>
                 </Box>
             </Flex>
             <Flex>
                 <Box
-                h={'350px'}
-                w={'200px'}
-                bg={'purple.100'}
-                boxShadow={'2xl'}
-                rounded={'md'}
-                p={6}
-                overflow={'hidden'}>
+                    h={'350px'}
+                    w={'200px'}
+                    bg={'purple.100'}
+                    boxShadow={'2xl'}
+                    rounded={'md'}
+                    p={6}
+                    overflow={'hidden'}>
                     {}
                     <Flex
-                    justifyContent="center"
-                    alignItems="center"
-                    h="216px"
-                    w="full"
-                    overflow="hidden">
+                        justifyContent="center"
+                        alignItems="center"
+                        h="216px"
+                        w="full"
+                        overflow="hidden">
                         <Box
-                        as="div"
-                        position="relative"
-                        w={size}
-                        h={size}
-                        _before={{
-                            content: "''",
-                            position: 'relative',
-                            display: 'block',
-                            width: '300%',
-                            height: '300%',
-                            boxSizing: 'border-box',
-                            marginLeft: '-100%',
-                            marginTop: '-100%',
-                            borderRadius: '50%',
-                            bgColor: color,
-                            animation: `2.25s ${pulseRing} cubic-bezier(0.455, 0.03, 0.515, 0.955) -0.4s infinite`,
-                        }}>
-                            <Avatar
-                            src="https://i.pravatar.cc/300"
-                            size="full"
-                            position="absolute"
-                            top={0}/>
-                            </Box>
-                        </Flex>
+                            as="div"
+                            position="relative"
+                            w={size}
+                            h={size}
+                            _before={{
+                                content: "''",
+                                position: 'relative',
+                                display: 'block',
+                                width: '300%',
+                                height: '300%',
+                                boxSizing: 'border-box',
+                                marginLeft: '-100%',
+                                marginTop: '-100%',
+                                borderRadius: '50%',
+                                bgColor: color,
+                                animation: `2.25s ${pulseRing} cubic-bezier(0.455, 0.03, 0.515, 0.955) -0.4s infinite`,
+                            }}>
+                                <Avatar
+                                    src="https://i.pravatar.cc/300"
+                                    size="full"
+                                    position="absolute"
+                                    top={0}/>
+                                </Box>
+                            </Flex>
                         <Button
-                backgroundColor={'purple'}
-                color={'white'}
-                onClick={(e) => {handleSubmit(e)}}
-                >
-                    Atualizar Dados
-                </Button>
-                </Box>
+                            fontFamily={'sen'}
+                            backgroundColor={'purple'}
+                            color={'white'}
+                            onClick={(e) => {handleSubmit(e)}}
+                            >
+                                Atualizar Dados
+                        </Button>
+                    </Box>
+                </Flex>
             </Flex>
-        </Flex>
         </Container>
-    );
+    </>);
 }
 
 export default Home;
