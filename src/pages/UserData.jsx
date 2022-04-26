8db6edbd349d11c5c7da8fd46eb19fd0a31c0a9d
Priscila Rossetto <priscilassouzap@gmail.com>
2022-04-26 17:44:46 -0300

    feat/user: estrutura da page UserData

--- src/pages/UserData.jsx
+++ src/pages/UserData.jsx
@@ -1,7 +1,51 @@
+import ContainerSection from '../components/Layouts/Container';
+import BoxSection from '../components/Layouts/Box';
+import HeadingTitle from '../components/HeadingTitle/HeadingTitle';
+import InputComponent from '../components/Input/InputComponent';
+import ButtonComponent from '../components/Button/ButtonComponent';
+
 function UserData() {
     return (
-        <h1>edit user data</h1>
-    );
+        <>
+        <ContainerSection>
+            <BoxSection>
+                <HeadingTitle>
+                    Atualizar Dados
+                </HeadingTitle>
+                <InputComponent
+                    id='fullName'
+                    type="text"
+                    placeholder={'Nome e Sobrenome'}
+                    onChange={(e) => {handle(e)}}
+                />
+                <InputComponent
+                    id='username'
+                    type="text"
+                    placeholder={'Username'}
+                    onChange={(e) => {handle(e)}}
+                />
+                <InputComponent
+                    id='password'
+                    type="password"
+                    placeholder={'Senha'}
+                    onChange={(e) => {handle(e)}}
+                />
+                <InputComponent
+                    id='password'
+                    type="password"
+                    placeholder={'Confirme a senha'}
+                    onChange={(e) => {handle(e)}}
+                />
+                <ButtonComponent
+                    backgroundColor={'purple'}
+                    color={'white'}
+                    onClick={(e) => {handleSubmit(e)}}
+                    >
+                Confirmar
+                </ButtonComponent>
+            </BoxSection>
+        </ContainerSection>
+    </>);
 }
 
 export default UserData;
